6f45719dd63f96d312d9378143e225ef86fb099e
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-10-31 18:47:43 +0000

    Proper counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,46 @@
 import React, { useState, useEffect } from 'react';
 
-function useTimedCounter() {
+function useTime(interval = 1000) {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(
+    () => {
+      console.log('setInterval 2');
+      const handle = setInterval(
+        () => setTime(new Date().toLocaleTimeString()),
+        interval
+      );
+      return () => {
+        console.log('clearInterval 2');
+        clearInterval(handle);
+      };
+    },
+    [interval]
+  );
+
+  return time;
+}
+
+function useTimedCounter(interval = 1000) {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log('setTimeout');
-    const handle = setTimeout(() => setCount(count + 1), 1000);
-    return () => {
-      console.log('clearTimeout');
-      clearTimeout(handle);
-    };
-  });
+  useEffect(
+    () => {
+      const handle = setTimeout(() => setCount(count + 1), interval);
+      return () => {
+        console.log('clearTimeout 1');
+        clearTimeout(handle);
+      };
+    },
+    [count]
+  );
 
   return [count, () => setCount(count + 10), () => setCount(count - 10)];
 }
 
 const Counter = () => {
-  const [count, increment, decrement] = useTimedCounter();
+  const [interval, setInterval] = useState(1000 * 5);
+  const [count, increment, decrement] = useTimedCounter(interval);
+  const time = useTime();
 
   return (
     <div>
@@ -23,7 +48,9 @@ const Counter = () => {
       <div>
         <button onClick={decrement}>Decrement</button>
         <button onClick={increment}>Increment</button>
+        <button onClick={() => setInterval(interval + 1000)}>Interval</button>
       </div>
+      {time}
     </div>
   );
 };
