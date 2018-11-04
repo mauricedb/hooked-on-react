38290ead2c1718a1d6a71ed8400a5b714c4162fb
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-11-04 12:53:54 +0100

    Counter using state hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,59 +1,12 @@
-import React, { useState, useEffect } from 'react';
-
-function useTime(interval = 1000) {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
-
-  useEffect(
-    () => {
-      // console.log('setInterval 2');
-      const handle = setInterval(
-        () => setTime(new Date().toLocaleTimeString()),
-        interval
-      );
-      return () => {
-        // console.log('clearInterval 2');
-        clearInterval(handle);
-      };
-    },
-    [interval]
-  );
-
-  return time;
-}
-
-function useTimedCounter(interval = 1000) {
-  const [count, setCount] = useState(0);
-  useEffect(
-    () => {
-      console.log('setInterval useTimedCounter');
-
-      const handle = setInterval(() => setCount(c => c + 1), interval);
-
-      return () => {
-        console.log('clearInterval useTimedCounter');
-        clearInterval(handle);
-      };
-    },
-    [interval]
-  );
-
-  return [count, () => setCount(count + 10), () => setCount(count - 10)];
-}
+import React, { useState } from 'react';
 
 const Counter = () => {
-  const [interval, setInterval] = useState(1000 * 2);
-  const [count, increment, decrement] = useTimedCounter(interval);
-  const time = useTime();
+  const [count, setCount] = useState(1);
 
   return (
     <div>
-      {count}
-      <div>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={increment}>Increment</button>
-        <button onClick={() => setInterval(interval + 1000)}>Interval</button>
-      </div>
-      {time}
+      <p>The count is: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 };
